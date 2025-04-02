81b18a20de8e3130110d37fb85c7c8391e27cc19
Ahmed Misbah <ahmed.misbah@arabiyya.edu.mv>
2025-04-02 17:28:20 +0500

    Extract Arabiyya.Theme as a submodule

--- Arabiyya.Theme/Navigation/Converters/AnyTrueConverter.cs
+++
@@ -1,8 +0,0 @@
-﻿using System.Globalization;
-using Avalonia.Data.Converters;
-
-namespace Arabiyya.Theme.Navigation.Converters;
-public class AnyTrueConverter : IMultiValueConverter
-{
-    public object Convert(IList<object?> values, Type targetType, object? parameter, CultureInfo culture) => values.OfType<bool>().Any(v => v);
-}
